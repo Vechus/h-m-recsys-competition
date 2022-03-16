fe73ee752f34a36feef00c82b7d1e9288652b353
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2022-03-16 18:15:39 +0100

    Updated the DataCleaning script

--- Data_cleaning/DataCleaning.py
+++ Data_cleaning/DataCleaning.py
@@ -12,12 +12,14 @@ if __name__ == '__main__':
     path = '../dataset'
 
     df_articles_raw = pd.read_csv(os.path.join(path, dataset_dict["articles"]))
-    #df_customers_raw = pd.read_csv(os.path.join(path, dataset_dict["customers"]))
-    #df_transactions_raw = pd.read_csv(os.path.join(path, dataset_dict["transactions"]))
+    df_customers_raw = pd.read_csv(os.path.join(path, dataset_dict["customers"]))
+    df_transactions_raw = pd.read_csv(os.path.join(path, dataset_dict["transactions"]))
 
-    #df_customers = initial_all_missing_values(df_customers_raw)
+    df_customers = initial_all_missing_values(df_customers_raw)
     df_articles = articles_func(df_articles_raw)
-    #df_transactions = transactions_cleaning(df_transactions_raw)
-    print(df_articles.head())
+    df_transactions = transactions_cleaning(df_transactions_raw)
+    #print(df_articles.head())
 
     df_articles.to_csv(os.path.join(path, "cleaned_"+dataset_dict["articles"]))
+    df_customers.to_csv(os.path.join(path, "cleaned_" + dataset_dict["customers"]))
+    df_transactions.to_csv(os.path.join(path, "cleaned_" + dataset_dict["transactions"]))
