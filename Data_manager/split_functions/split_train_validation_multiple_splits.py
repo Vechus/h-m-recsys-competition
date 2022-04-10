496a60077467c9486fda57cd9068bd23230a475a
RiccardoPazzi <riccardo.pazzi@mail.polimi.it>
2022-04-10 18:41:41 +0200

    Added possibility to split URM in multiple times using random holdout, added evaluation class EvaluatorMultipleURMs which allows to gather statistics about MAP on multiple splits

---
+++ Data_manager/split_functions/split_train_validation_multiple_splits.py
@@ -0,0 +1,44 @@
+""""
+Created 09/04/22
+@author: Riccardo Pazzi
+"""
+
+from Data_manager.split_functions.split_train_validation_random_holdout import split_train_in_two_percentage_user_wise, \
+    split_train_in_two_percentage_global_sample
+
+
+def split_multiple_times(URM_all, number_of_splits=1, train_percentage=0.1, is_global=True, keep_only_test=False):
+    """
+    The function splits an URM in multiple matrices selecting the number of interactions globally
+    :param URM_all:
+    :param number_of_splits: Number of random (train, test) pairs of matrices
+    :param train_percentage:
+    :param is_global: If True divides URM on he whole dataset, if False divides user-wise
+    :param keep_only_test: If True discards train URMs in the returned list
+    :return: list of TRAIN/TEST URMs or TEST URMs with selected train_percentage
+    """
+
+    train_test_list = []
+
+    if not is_global:
+        for k in range(number_of_splits):
+
+            train_URM, test_URM = split_train_in_two_percentage_user_wise(URM_all, train_percentage=train_percentage)
+            if keep_only_test:
+                train_test_list.append(test_URM)
+            else:
+                train_test_list.append((train_URM, test_URM))
+
+    else:
+        for k in range(number_of_splits):
+            train_URM, test_URM = split_train_in_two_percentage_global_sample(URM_all,
+                                                                              train_percentage=train_percentage)
+
+            if keep_only_test:
+                train_test_list.append(test_URM)
+            else:
+                train_test_list.append((train_URM, test_URM))
+
+    return train_test_list
+
+
