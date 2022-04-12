7630a998f035e584f3820de3e6311ac8210b48b3
Luca Vecchio <luca1.vecchio@mail.polimi.it>
2022-04-12 15:56:48 +0200

    prepare IALS run

--- run_hyperparameter_search_dataCleaningPipeline.py
+++ run_hyperparameter_search_dataCleaningPipeline.py
@@ -110,21 +110,21 @@ def read_data_split_and_search():
                                                        parallelizeKNN=False)
 
     #
-    #pool = multiprocessing.Pool(processes=int(multiprocessing.cpu_count()), maxtasksperchild=1)
-    #pool.map(runParameterSearch_Collaborative_partial, collaborative_algorithm_list)
+    pool = multiprocessing.Pool(processes=int(multiprocessing.cpu_count()), maxtasksperchild=1)
+    pool.map(runParameterSearch_Collaborative_partial, collaborative_algorithm_list)
 
     #
     #
-    for recommender_class in collaborative_algorithm_list:
-
-        try:
-
-            runParameterSearch_Collaborative_partial(recommender_class)
-
-        except Exception as e:
-
-            print("On recommender {} Exception {}".format(recommender_class, str(e)))
-            traceback.print_exc()
+    #for recommender_class in collaborative_algorithm_list:
+    #
+    #    try:
+    #
+    #        runParameterSearch_Collaborative_partial(recommender_class)
+    #
+    #    except Exception as e:
+    #
+    #        print("On recommender {} Exception {}".format(recommender_class, str(e)))
+    #        traceback.print_exc()
 
     exit()
 
