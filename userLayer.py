3aec05942e27d39639e7ba965387bb6d11b88c0e
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2022-04-21 16:28:47 +0200

    Update userLayer.py

--- userLayer.py
+++ userLayer.py
@@ -20,6 +20,9 @@ dataset = reader.load_data('{}/processed_URM_20190622_20190923/{}/'.format(DATAS
 ICM_name = "ICM_idxgrp_idx_prdtyp"
 ICM_train = dataset.get_ICM_from_name(ICM_name)
 
+UCM_name = "UCM_age"
+UCM_train = dataset.get_loaded_UCM_dict()[UCM_name]
+
 URM_train = dataset.get_URM_from_name('URM_train')
 URM_validation = dataset.get_URM_from_name('URM_validation')
 URM_test = URM_validation
@@ -28,19 +31,20 @@ evaluator_validation = EvaluatorHoldout(URM_validation, cutoff_list=[12])
 evaluator_test = EvaluatorHoldout(URM_validation, cutoff_list=[12])
 
 profile_length = np.ediff1d(sps.csr_matrix(URM_train).indptr)
+profile_length = [x for x in profile_length if x > 0]
 
-block_size = int(len(profile_length) * 0.05)
+block_size = int(len(profile_length) * 0.2)
 sorted_users = np.argsort(profile_length)
 
-print(block_size, sorted_users)
+print(block_size, sorted_users,max(profile_length))
 
-for group_id in range(0, 20):
+for group_id in range(0, 5):
     start_pos = group_id * block_size
     end_pos = min((group_id + 1) * block_size, len(profile_length))
 
     users_in_group = sorted_users[start_pos:end_pos]
 
-    users_in_group_p_len = profile_length[users_in_group]
+    users_in_group_p_len = np.array(profile_length)[users_in_group]
 
     print("Group {}, #users in group {}, average p.len {:.2f}, median {}, min {}, max {}".format(
         group_id,
@@ -63,6 +67,8 @@ from Recommenders.MatrixFactorization.Cython.MatrixFactorization_Cython import M
 from Recommenders.MatrixFactorization.PureSVDRecommender import PureSVDRecommender
 from Recommenders.KNN.ItemKNN_CFCBF_Hybrid_Recommender import ItemKNN_CFCBF_Hybrid_Recommender
 
+from Recommenders.KNN.UserKNN_CFCBF_Hybrid_Recommender import UserKNN_CFCBF_Hybrid_Recommender
+
 MAP_recommender_per_group = {}
 
 collaborative_recommender_class = {
@@ -75,7 +81,8 @@ collaborative_recommender_class = {
     "PureSVD": PureSVDRecommender,
 }
 
-hybird_recommender_class = {"ItemKNNCFCBF": ItemKNN_CFCBF_Hybrid_Recommender
+hybird_recommender_class = { "ItemKNNCFCBF": ItemKNN_CFCBF_Hybrid_Recommender,
+                            "UserKNNCFCBF": UserKNN_CFCBF_Hybrid_Recommender
                             }
 
 recommender_object_dict = {}
@@ -86,20 +93,25 @@ for label, recommender_class in collaborative_recommender_class.items():
     recommender_object_dict[label] = recommender_object
 
 for label, recommender_class in hybird_recommender_class.items():
-    recommender_object = recommender_class(URM_train, ICM_train)
-    recommender_object.fit()
-    recommender_object_dict[label] = recommender_object
+    if label=="ItemKNNCFCBF":
+        recommender_object = recommender_class(URM_train, ICM_train)
+        recommender_object.fit()
+        recommender_object_dict[label] = recommender_object
+    else:
+        recommender_object = recommender_class(URM_train, UCM_train)
+        recommender_object.fit()
+        recommender_object_dict[label] = recommender_object
 
 cutoff = 12
 
-for group_id in range(0, 20):
+for group_id in range(0, 5):
 
     start_pos = group_id * block_size
     end_pos = min((group_id + 1) * block_size, len(profile_length))
 
     users_in_group = sorted_users[start_pos:end_pos]
 
-    users_in_group_p_len = profile_length[users_in_group]
+    users_in_group_p_len = np.array(profile_length)[users_in_group]
 
     print("Group {}, #users in group {}, average p.len {:.2f}, median {}, min {}, max {}".format(
         group_id,
@@ -132,3 +144,5 @@ plt.xlabel('User Group')
 plt.legend()
 plt.show()
 plt.savefig(os.path.join(DATASET_PATH, 'userwise.png'))
+
+
