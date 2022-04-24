a2741548521808ab9856f3877c3e92888df42e9f
Luca Vecchio <luca1.vecchio@mail.polimi.it>
2022-04-24 16:16:00 +0200

    generate URM for ItemKNNCFCBF

--- DataProcessing/process_data_script.py
+++ DataProcessing/process_data_script.py
@@ -38,8 +38,8 @@ if __name__ == '__main__':
     # timestamp_list_train = [("2019-06-22", "2019-09-23")]
     # timestamp_list_validation = [("2019-09-23", "2019-09-30")]
 
-    timestamp_list_train = [("2018-09-20", "2020-09-16")]
-    timestamp_list_validation = [("2020-09-16", "2020-09-23")]
+    timestamp_list_train = [("2019-06-22", "2019-09-23")]
+    timestamp_list_validation = [("2019-09-23", "2019-09-30")]
     split_train_validation_multiple_intervals(manager, transactions, timestamp_list_train, timestamp_list_validation)
 
     # URM_train for submission
@@ -53,7 +53,7 @@ if __name__ == '__main__':
     dataset = manager.generate_Dataset(DATASET_NAME, True)
 
     # PROCESSED_PATH = os.getenv('PROCESSED_PATH')
-    dataset.save_data('{}/processed_URM_train_20180920_20200916_val_20200916_20200923/{}/'.format(DATASET_PATH, DATASET_NAME))
+    dataset.save_data('{}/processed_train-20190622_20190923-val-20190923_20190930/{}/'.format(DATASET_PATH, DATASET_NAME))
 
     dataset.print_statistics()
     dataset.print_statistics_global()
