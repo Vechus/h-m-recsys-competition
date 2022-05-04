de373fce906996f534fc4d6ec76efbcc4385583f
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2022-05-04 17:25:41 +0200

    Update LgbmRanker.py

--- Data_cleaning/LgbmRanker.py
+++ Data_cleaning/LgbmRanker.py
@@ -517,7 +517,7 @@ if __name__ == "__main__":
     print(df_imps.head(30))
     print(df_imps.to_csv(os.path.join(path, "feature_importance.csv")))
 
-    # sample_sub = pd.read_csv(os.path.join(path, 'sample_submission.csv'))
+    sample_sub = pd.read_csv(os.path.join(path, 'sample_submission.csv'))
     #
     # df = pd.read_csv(os.path.join(path, "submission_toppop_weight_decay.csv"))
     # print("start")
@@ -528,15 +528,15 @@ if __name__ == "__main__":
     # )
     # print("end")
 
-    # candidates = prepare_candidates(sample_sub.customer_id.unique(), 12)
+    candidates = prepare_candidates(sample_sub.customer_id.unique(), 12)
     # print(candidates.shape)
     # print(df.shape)
     #
-    # candidates = (
-    #     candidates
-    #         .merge(user_features, on=('customer_id'))
-    #         .merge(item_features, on=('article_id'))
-    # )
+    candidates = (
+        candidates
+            .merge(user_features_validation, on=('customer_id'))
+            .merge(item_features_validation, on=('article_id'))
+    )
     #
     # print("candidates merged.")
 
@@ -554,37 +554,37 @@ if __name__ == "__main__":
     # print("concat.")
     # # candidates = candidates.drop_duplicates()
     #
-    # preds = []
-    # batch_size = 1000000
-    # for bucket in tqdm(range(0, len(candidates), batch_size)):
-    #     outputs = ranker.predict(
-    #         candidates.iloc[bucket: bucket + batch_size]
-    #             .drop(columns=['customer_id', 'article_id'])
-    #     )
-    #     preds.append(outputs)
-    #
-    # print("pred done.")
-    #
-    # preds = np.concatenate(preds)
-    # candidates['preds'] = preds
-    # preds = candidates[['customer_id', 'article_id', 'preds']]
-    # preds.sort_values(['customer_id', 'preds'], ascending=False, inplace=True)
-    # preds = (
-    #     preds
-    #         .groupby('customer_id')[['article_id']]
-    #         .aggregate(lambda x: x.tolist())
-    # )
-    # preds['article_id'] = preds['article_id'].apply(
-    #     lambda x: ' '.join([str(v) for k, v in enumerate(set(x)) if k < 12]))
-    #
-    # print("select 12 done.")
-    #
-    # preds = sample_sub[['customer_id']].merge(
-    #     preds
-    #         .reset_index()
-    #         .rename(columns={'article_id': 'prediction'}), how='left')
-    # preds['prediction'].fillna(' '.join([str(art) for art in dummy_list_2w]), inplace=True)
-    #
-    # print("add customer_id done.")
-    #
-    # preds.to_csv(os.path.join(path, 'submission_ranking_0504.csv'), index=False)
+    preds = []
+    batch_size = 1000000
+    for bucket in tqdm(range(0, len(candidates), batch_size)):
+        outputs = ranker.predict(
+            candidates.iloc[bucket: bucket + batch_size]
+                .drop(columns=['customer_id', 'article_id']), num_iteration=ranker.best_iteration_
+        )
+        preds.append(outputs)
+
+    print("pred done.")
+
+    preds = np.concatenate(preds)
+    candidates['preds'] = preds
+    preds = candidates[['customer_id', 'article_id', 'preds']]
+    preds.sort_values(['customer_id', 'preds'], ascending=False, inplace=True)
+    preds = (
+        preds
+            .groupby('customer_id')[['article_id']]
+            .aggregate(lambda x: x.tolist())
+    )
+    preds['article_id'] = preds['article_id'].apply(
+        lambda x: ' '.join([str(v) for k, v in enumerate(set(x)) if k < 12]))
+
+    print("select 12 done.")
+
+    preds = sample_sub[['customer_id']].merge(
+        preds
+            .reset_index()
+            .rename(columns={'article_id': 'prediction'}), how='left')
+    preds['prediction'].fillna(' '.join([str(art) for art in dummy_list_2w]), inplace=True)
+
+    print("add customer_id done.")
+
+    preds.to_csv(os.path.join(path, 'submission_ranking_0504.csv'), index=False)
